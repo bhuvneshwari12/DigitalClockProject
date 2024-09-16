ad0c1f56121d029de94f06cc1590c15263c3ed00
bhuvneshwari12 <148437144+bhuvneshwari12@users.noreply.github.com>
2024-09-16 23:58:20 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+import './App.css';
+
+const App = () => {
+  const [dateTime, setDateTime] = useState({
+    time: new Date().toLocaleTimeString(),
+    date: new Date().toLocaleDateString(),
+    day: new Date().toLocaleDateString('en-US', { weekday: 'long' })
+  });
+
+  useEffect(() => {
+    const timerId = setInterval(() => {
+      const now = new Date();
+      setDateTime({
+        time: now.toLocaleTimeString(),
+        date: now.toLocaleDateString(),
+        day: now.toLocaleDateString('en-US', { weekday: 'long' })
+      });
+    }, 1000);
+    return () => clearInterval(timerId);
+  }, []);
+
+  return (
+    <div className="main-container">
+      <div className="clock-container">
+        <div className="digital-clock">{dateTime.time}</div>
+        <div className="date-day">
+          <div className="date">
+            <i className="fas fa-calendar-day icon"></i> {dateTime.date}
+          </div>
+          <div className="day">
+            <i className="fas fa-sun icon"></i> {dateTime.day}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
